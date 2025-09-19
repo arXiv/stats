6fb99645e4b4db1ba60c3be45cc343553d602478
carly <carly@arxiv.org>
2025-09-19 13:43:22 -0400

    move main db entities

--- stats-entities/stats_entities/site_usage.py
+++ stats-entities/stats_entities/site_usage.py
@@ -10,29 +10,12 @@ from sqlalchemy.orm import (
     declarative_base,
 )
 
-ReadBase = declarative_base()
-WriteBase = declarative_base()
+SiteUsageBase = declarative_base()
 
 
-class DocumentCategory(ReadBase):
-    __tablename__ = "arXiv_document_category"
-
-    document_id = Column(Integer, primary_key=True, nullable=False, index=True)
-    category = Column(String, primary_key=True, nullable=False, index=True)
-    is_primary = Column(Integer, nullable=False)
-
-
-class Metadata(ReadBase):
-    __tablename__ = "arXiv_metadata"
-
-    metadata_id = Column(Integer, primary_key=True)
-    document_id = Column(Integer, nullable=False, index=True)
-    paper_id = Column(String(64), nullable=False)
-    is_current = Column(Integer)
-
-
-class HourlyDownloads(WriteBase):
+class HourlyDownloads(SiteUsageBase):
     __tablename__ = "hourly_downloads"
+
     country = Column(String(255), primary_key=True)
     download_type = Column(
         String(16),
@@ -44,6 +27,7 @@ class HourlyDownloads(WriteBase):
     primary_count = Column(Integer)
     cross_count = Column(Integer)
     start_dttm = Column(DateTime, primary_key=True)
+
     __table_args__ = (
         PrimaryKeyConstraint("country", "download_type", "category", "start_dttm"),
     )
