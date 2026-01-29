a2698a3e2f73b59756df3925f366773578a4dea8
carly <carly@arxiv.org>
2026-01-29 15:55:15 -0500

    use computed field

--- stats-functions/stats_functions/config.py
+++ stats-functions/stats_functions/config.py
@@ -1,7 +1,6 @@
 from typing import Optional
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import field_validator
-from pydantic_core.core_schema import ValidationInfo
+from pydantic import computed_field
 
 
 class BaseConfig(BaseSettings):
@@ -23,16 +22,16 @@ class DatabaseConfig(BaseConfig):
 
 class FunctionConfig(BaseConfig):
     env: str = "DEV"
-    project: Optional[str] = None
     log_level: str = "INFO"
     log_locally: bool = False
     max_event_age_in_minutes: Optional[int] = None
 
-    @field_validator("project", mode="before")
-    def set_project(cls, v, info: ValidationInfo):
+    @computed_field
+    @property
+    def project(self) -> str:
         project_map = {
             "DEV": "arxiv-development",
             "PROD": "arxiv-production",
         }
 
-        return project_map[info.data.get("env")]
+        return project_map[self.env]
