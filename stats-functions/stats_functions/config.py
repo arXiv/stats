16ca9c81b40bb561fa3c9dfe5f7f47aa291a4f37
carly <carly@arxiv.org>
2026-01-30 11:15:12 -0500

    update config

--- stats-functions/stats_functions/config.py
+++ stats-functions/stats_functions/config.py
@@ -1,6 +1,5 @@
 from typing import Optional
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import computed_field
 
 
 class BaseConfig(BaseSettings):
@@ -21,17 +20,8 @@ class DatabaseConfig(BaseConfig):
 
 
 class FunctionConfig(BaseConfig):
-    env: str = "DEV"
+    env: str
+    project: Optional[str] = None
     log_level: str = "INFO"
     log_locally: bool = False
     max_event_age_in_minutes: Optional[int] = None
-
-    @computed_field
-    @property
-    def project(self) -> str:
-        project_map = {
-            "DEV": "arxiv-development",
-            "PROD": "arxiv-production",
-        }
-
-        return project_map[self.env]
