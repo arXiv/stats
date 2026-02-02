087ea8a37bc2cee47545db0b38db691c15977159
carly <carly@arxiv.org>
2026-02-02 15:58:24 -0500

    update config

--- stats-functions/monthly_downloads/src/config.py
+++ stats-functions/monthly_downloads/src/config.py
@@ -1,20 +1,25 @@
 from typing import Optional
 from stats_functions.config import FunctionConfig, DatabaseConfig
 
+
 class Config(FunctionConfig):
     db: Optional[DatabaseConfig] = None
 
     max_event_age_in_minutes: int = 50
 
+
 class TestConfig(Config):
     log_level: str = "DEBUG"
     log_locally: bool = True
 
+
 class DevConfig(Config):
     log_level: str = "DEBUG"
 
+
 class ProdConfig(Config):
     pass
+
 
 config_map = {
     "TEST": TestConfig,
@@ -22,6 +27,6 @@ config_map = {
     "PROD": ProdConfig,
 }
 
+
 def get_config(environment: str) -> Config:
-    config_class = config_map[environment]
-    return config_class()
+    return config_map[environment]()
