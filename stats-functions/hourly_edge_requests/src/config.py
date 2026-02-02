4407822890d574fc07ea5600570c91eb9cdb42f2
carly <carly@arxiv.org>
2026-02-02 16:17:47 -0500

    update hourly requests wip

---
+++ stats-functions/hourly_edge_requests/src/config.py
@@ -0,0 +1,37 @@
+from typing import Optional
+from stats_functions.config import FunctionConfig, DatabaseConfig
+
+
+class Config(FunctionConfig):
+    db: Optional[DatabaseConfig] = None
+
+    max_event_age_in_minutes: int = 50
+    fastly_service_id: dict = {"arxiv.org": "umpGzwE2hXfa2aRXsOQXZ4"}
+    fastly_node_number: int = 0  # existing convention, corresponds to 'fastly'
+    hour_delay: int = 1
+
+    fastly_api_token: str
+
+
+class TestConfig(Config):
+    log_level: str = "DEBUG"
+    log_locally: bool = True
+
+
+class DevConfig(Config):
+    log_level: str = "DEBUG"
+
+
+class ProdConfig(Config):
+    pass
+
+
+config_map = {
+    "TEST": TestConfig,
+    "DEV": DevConfig,
+    "PROD": ProdConfig,
+}
+
+
+def get_config(environment: str) -> Config:
+    return config_map[environment]()
--- stats-functions/hourly_edge_requests/src/main.py
+++ stats-functions/hourly_edge_requests/src/main.py
@@ -1,260 +1,161 @@
 import os
-import argparse
 import logging
-from google.cloud import logging as gc_logging
-
-from datetime import datetime, timedelta, timezone
-from dateutil import parser
+from datetime import date, datetime
+from dateutil.relativedelta import relativedelta
 
 import functions_framework
 from cloudevents.http import CloudEvent
-from google.cloud.sql.connector import Connector, IPTypes
-import pymysql
+from google.cloud.sql.connector import Connector
+from google.cloud.sql.connector import IPTypes
 
 import fastly
 from fastly.api import stats_api
 from fastly.model.stats import Stats
 
-from sqlalchemy import create_engine, delete
-from sqlalchemy.engine.base import Engine
+from sqlalchemy import select
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import func
 
-from stats_entities.site_usage import HourlyRequests
-from models import Database, FastlyStatsApiResponse
+from config import get_config
+from models import FastlyStatsApiResponse
 from pydantic import ValidationError
 
+from stats_entities.site_usage import HourlyRequests
+from stats_functions.exception import NoRetryError
+from stats_functions.utils import (
+    set_up_cloud_logging,
+    get_engine,
+    event_time_exceeds_retry_window,
+    parse_cloud_event_time,
+)
+
+config = get_config(os.getenv("ENV"))
+
+logging.basicConfig(level=config.log_level)
 logger = logging.getLogger(__name__)
 
+set_up_cloud_logging(config)
 
-class NoRetryError(Exception):
-    pass
+SessionFactory = None
+
+if config.env != "TEST":
+    connector = Connector(ip_type=IPTypes.PUBLIC, refresh_strategy="LAZY")
+    SessionFactory = sessionmaker(bind=get_engine(connector, config.db))
 
 
-class HourlyEdgeRequestsJob:
-    MAX_EVENT_AGE_IN_MINUTES = 50  # to limit retries
-    FASTLY_SERVICE_ID = {"arxiv.org": "umpGzwE2hXfa2aRXsOQXZ4"}
-    FASTLY_NODE_NUMBER = 0  # existing convention, corresponds to 'fastly'
+def get_timestamps(hour: datetime) -> tuple[int, int]:
+    """transform datetime into start and end unix/epoch timestamps"""
+    start_time = int(hour.replace(minute=0, second=0, microsecond=0).timestamp())
+    end_time = int(hour.replace(minute=59, second=59, microsecond=0).timestamp())
 
-    def __init__(self):
-        self.hour_delay = os.getenv("HOUR_DELAY", 1)
+    return start_time, end_time
 
-        self.env = os.getenv("ENV")
-        self.write_db = Database(
-            os.getenv("WRITE_DB_INSTANCE"),
-            os.getenv("WRITE_DB_USER"),
-            os.getenv("WRITE_DB_PW"),
-            os.getenv("WRITE_DB_NAME"),
-        )
-        self.project = "arxiv-production" if self.env == "PROD" else "arxiv-development"
-        self.ip_type = (
-            IPTypes.PRIVATE if os.environ.get("PRIVATE_IP") else IPTypes.PUBLIC
-        )
-        self.log_level = os.getenv("LOG_LEVEL", "INFO")
-        self.log_locally = os.getenv("LOG_LOCALLY", False)
 
-        self.cloud_logging_client = None
-        self.write_connector = None
-
-        self._set_up_logging()
-        logger.info("Initialization of hourly edge requests job complete")
-
-    def _set_up_logging(self):
-        logging.basicConfig()
-        logger.setLevel(self.log_level)
-
-        if not (self.log_locally):
-            logger.info("Initializing cloud logging")
-            self.cloud_logging_client = gc_logging.Client(project=self.project)
-            self.cloud_logging_client.setup_logging()
-
-    @staticmethod
-    def instantiate_connection_pool(ip_type, db: Database) -> tuple[Connector, Engine]:
-        connector = Connector(ip_type=ip_type, refresh_strategy="LAZY")
-
-        def getconn() -> pymysql.connections.Connection:
-            conn: pymysql.connections.Connection = connector.connect(
-                db.instance_name,
-                "pymysql",
-                user=db.username,
-                password=db.password,
-                db=db.db_name,
-            )
-            return conn
-
-        pool = create_engine("mysql+pymysql://", creator=getconn)
-
-        return connector, pool
-
-    def _configure_fastly(self):
-        self.fastly_config = fastly.Configuration()
-        self.fastly_config.api_token = os.getenv("FASTLY_TOKEN")
-
-        if not self.fastly_config.api_token:
-            logger.error("Fastly API token missing! Check environment configuration")
-            raise NoRetryError
-
-    def _get_timestamps(self, hour: datetime) -> tuple[int, int]:
-        """transform datetime into start and end unix/epoch timestamps"""
-        start_time = int(hour.replace(minute=0, second=0, microsecond=0).timestamp())
-        end_time = int(hour.replace(minute=59, second=59, microsecond=0).timestamp())
-
-        return start_time, end_time
-
-    def get_fastly_stats(self, hour: datetime) -> Stats:
-        start_time, end_time = self._get_timestamps(hour)
-
-        with fastly.ApiClient(self.fastly_config) as client:
-            api_instance = stats_api.StatsApi(client)
-            options = {
-                "service_id": self.FASTLY_SERVICE_ID["arxiv.org"],
-                "start_time": start_time,
-                "end_time": end_time,
-            }
-
-            response = api_instance.get_service_stats(**options)
-
-            try:
-                return FastlyStatsApiResponse(**response.to_dict())
-            except ValidationError:
-                logger.exception(
-                    "Could not validate response payload! Check response format"
-                )
-                raise NoRetryError
-
-    def sum_requests(self, response: FastlyStatsApiResponse) -> int:
-        return sum(response.stats[pop].edge_requests for pop in response.stats.keys())
-
-    def write_to_db(self, hour: datetime, request_count: int):
-        self.write_connector, write_pool = self.instantiate_connection_pool(
-            self.ip_type, self.write_db
-        )
-        Session = sessionmaker(bind=write_pool)
-
-        with Session() as session:
-            logger.info("Beginning write database session")
-
-            # mark existing rows for deletion
-            delete_stmt = delete(HourlyRequests).where(
-                HourlyRequests.start_dttm == hour
-            )
-            result = session.execute(delete_stmt)
-            logger.info(f"Number of existing rows to be deleted: {result.rowcount}")
-
-            # add new data to be inserted to the session queue
-            new_hourly_requests = HourlyRequests(
-                start_dttm=hour, source_id=0, request_count=request_count
-            )
-            session.add(new_hourly_requests)
-            logger.info(f"Requests for hour {hour}: {request_count}")
-
-            # commit both the deletion and the insertion as a single transaction
-            # will rollback both if one fails
-            session.commit()
-
-        logger.info("Write database transaction successfully committed; session closed")
-
-    def _event_time_exceeds_retry_window(self, event_time: datetime) -> bool:
-        """Prevent infinite retries by dismissing event timestamps that are too old"""
-        current_time = datetime.now(timezone.utc)
-        max_event_age = timedelta(minutes=self.MAX_EVENT_AGE_IN_MINUTES)
-
-        if (current_time - event_time) > max_event_age:
-            return True
-        else:
-            return False
-
-    def _validate_cloud_event(self, cloud_event: CloudEvent) -> datetime:
-        # take a timezone-aware RFC 3339 string and parse to a datetime object
-        # assumes utc timezone
-        event_time = parser.isoparse(cloud_event["time"]).replace(tzinfo=timezone.utc)
-
-        if self._event_time_exceeds_retry_window(event_time):
-            raise NoRetryError
-
-        # subtract delay
-        return event_time - timedelta(hours=self.hour_delay)
-
-    def _validate_date(self, hour: str) -> datetime:
-        date_format = "%Y-%m-%d%H"
+def get_fastly_stats(start_time: int, end_time: int) -> Stats:
+    with fastly.ApiClient(fastly.Configuration()) as client:
+        api_instance = stats_api.StatsApi(client)
+        options = {
+            "service_id": config.fastly_service_id,
+            "start_time": start_time,
+            "end_time": end_time,
+        }
+        response = api_instance.get_service_stats(**options)
 
         try:
-            assert len(hour) == 12
-            valid_hour = datetime.strptime(hour, date_format)
-
-            # assumes utc timezone
-            return valid_hour.replace(tzinfo=timezone.utc)
-
-        except (AssertionError, ValueError):
-            logger.error("Invalid input time! Check input")
-            raise NoRetryError
-
-    def _validate_inputs(
-        self,
-        cloud_event: CloudEvent = None,
-        hour: str = None,
-    ) -> datetime:
-        try:
-            if cloud_event:
-                logger.info("Received cloud event trigger")
-                hour = self._validate_cloud_event(cloud_event)
-            elif hour:
-                logger.info("Received hour input")
-                hour = self._validate_date(hour)
-            else:
-                logger.error("Must receive either a cloud event or valid hour input!")
-                raise NoRetryError
-
-        except NoRetryError:
-            raise
-
-        # round down to the start of the hour
-        hour = hour.replace(minute=0, second=0, microsecond=0)
-
-        logger.info(f"Query parameters for fastly: hour={hour}")
-        return hour
-
-    def _cleanup(self):
-        if self.cloud_logging_client:
-            self.cloud_logging_client.close()
-
-        if self.write_connector:
-            self.write_connector.close()
-
-    def run(self, cloud_event: CloudEvent = None, hour: str = None):
-        try:
-            hour = self._validate_inputs(cloud_event=cloud_event, hour=hour)
-            self._configure_fastly()
-            response = self.get_fastly_stats(hour)
-            request_count = self.sum_requests(response)
-            self.write_to_db(hour, request_count)
-
-        except NoRetryError:
+            return FastlyStatsApiResponse(**response.to_dict())
+        except ValidationError:
             logger.exception(
-                "A NoRetry exception has been raised! Will not retry. Fix the problem and manually run the function to patch data as needed."
+                "Could not validate response payload! Check response format"
             )
-            return
+            raise NoRetryError
 
-        finally:
-            self._cleanup()
-            logger.info("Cleanup complete")
+
+def sum_requests(response: FastlyStatsApiResponse) -> int:
+    return sum(response.stats[pop].edge_requests for pop in response.stats.keys())
+
+
+def write_to_db(hour: datetime, count: int):
+    with SessionFactory() as session:
+        logger.info("Beginning write database session")
+
+        delete_stmt = delete(HourlyRequests).where(HourlyRequests.start_dttm == hour)
+        result = session.execute(delete_stmt)
+        logger.info(f"Number of existing rows to be deleted: {result.rowcount}")
+
+        new_hourly_requests = HourlyRequests(
+            start_dttm=hour, source_id=0, request_count=count
+        )
+        session.add(new_hourly_requests)
+        logger.info(f"Requests for hour {hour}: {count}")
+
+        # commit both the deletion and the insertion as a single transaction
+        session.commit()
+
+    logger.info("Write database transaction successfully committed; session closed")
+
+
+# def _validate_cloud_event(self, cloud_event: CloudEvent) -> datetime:
+#     # take a timezone-aware RFC 3339 string and parse to a datetime object
+#     # assumes utc timezone
+#     event_time = parser.isoparse(cloud_event["time"]).replace(tzinfo=timezone.utc)
+
+#     if self._event_time_exceeds_retry_window(event_time):
+#         raise NoRetryError
+
+#     # subtract delay
+#     return event_time - timedelta(hours=self.hour_delay)
+
+# def _validate_date(self, hour: str) -> datetime:
+#     date_format = "%Y-%m-%d%H"
+
+#     try:
+#         assert len(hour) == 12
+#         valid_hour = datetime.strptime(hour, date_format)
+
+#         # assumes utc timezone
+#         return valid_hour.replace(tzinfo=timezone.utc)
+
+#     except (AssertionError, ValueError):
+#         logger.error("Invalid input time! Check input")
+#         raise NoRetryError
+
+# def _validate_inputs(
+#     self,
+#     cloud_event: CloudEvent = None,
+#     hour: str = None,
+# ) -> datetime:
+#     try:
+#         if cloud_event:
+#             logger.info("Received cloud event trigger")
+#             hour = self._validate_cloud_event(cloud_event)
+#         elif hour:
+#             logger.info("Received hour input")
+#             hour = self._validate_date(hour)
+#         else:
+#             logger.error("Must receive either a cloud event or valid hour input!")
+#             raise NoRetryError
+
+#     except NoRetryError:
+#         raise
+
+#     # round down to the start of the hour
+#     hour = hour.replace(minute=0, second=0, microsecond=0)
+
+#     logger.info(f"Query parameters for fastly: hour={hour}")
+#     return hour
 
 
 @functions_framework.cloud_event
 def get_hourly_edge_requests(cloud_event: CloudEvent):
-    job = HourlyEdgeRequestsJob()
-    job.run(cloud_event=cloud_event)
+    try:
+        hour = validate_inputs(cloud_event)
+        start_time, end_time = get_timestamps(hour)
+        response = get_fastly_stats(start_time, end_time)
+        count = sum_requests(response)
+        write_to_db(hour, count)
 
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--hour",
-        dest="hour",
-        help="The hour to be processed, UTC, in 'YYYY-MM-DDHH' format",
-        required=True,
-    )
-
-    args = parser.parse_args()
-
-    job = HourlyEdgeRequestsJob()
-    job.run(hour=args.hour)
+    except NoRetryError:
+        logger.exception(
+            "A NoRetry exception has been raised! Will not retry. Fix the problem and manually run the function to patch data as needed."
+        )
+        return
