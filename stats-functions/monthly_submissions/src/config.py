297f1295e96f5316ceabb6503cecf08d7bffe702
carly <carly@arxiv.org>
2026-02-02 13:18:13 -0500

    update deps, add config

---
+++ stats-functions/monthly_submissions/src/config.py
@@ -0,0 +1,28 @@
+from typing import Optional
+from stats_functions.config import FunctionConfig, DatabaseConfig
+
+class Config(FunctionConfig):
+    read_db: Optional[DatabaseConfig] = None
+    write_db: Optional[DatabaseConfig] = None
+
+    max_event_age_in_minutes: int = 50
+
+class TestConfig(Config):
+    log_level: str = "DEBUG"
+    log_locally: bool = True
+
+class DevConfig(Config):
+    log_level: str = "DEBUG"
+
+class ProdConfig(Config):
+    pass
+
+config_map = {
+    "TEST": TestConfig,
+    "DEV": DevConfig,
+    "PROD": ProdConfig,
+}
+
+def get_config(environment: str) -> Config:
+    config_class = config_map[environment]
+    return config_class()
