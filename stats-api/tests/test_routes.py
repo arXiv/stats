8fd9921200e8beef412f148ed142abab6a9984b8
carly <carly@arxiv.org>
2026-03-05 16:26:43 -0500

    update tests

--- stats-api/tests/test_routes.py
+++ stats-api/tests/test_routes.py
@@ -1,44 +1,43 @@
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from http import HTTPStatus
+from datetime import datetime, date
+
+from stats_api.models import TodayPageData
 
 
 @patch("stats_api.service.StatsService.get_today_page_data")
 def test_today_route_success(mock_service, client):
-    mock_data = MagicMock()
-    mock_data.model_dump.return_value = {"some_key": "some_value"}
-    mock_service.return_value = mock_data
+    mock_service.return_value = TodayPageData(
+        arxiv_current_time=datetime.now(),
+        arxiv_requested_date=date(2026, 3, 3),
+        arxiv_timezone="",
+        total_requests=10,
+    )
 
-    response = client.get("/stats/today?date=2024-03-05") # TODO check format
+    response = client.get("/stats/today?date=20260303")
 
     assert response.status_code == HTTPStatus.OK
-    assert b"some_value" in response.data
 
 
 @patch("stats_api.service.StatsService.get_monthly_downloads")
 def test_get_monthly_downloads_csv_success(mock_service, client):
-    mock_service.return_value = "month,count\n2024-01,500"
+    mock_service.return_value = "month,count\n2026-03-01,500"
 
-    response = client.get(
-        "/stats/get_monthly_downloads?latest_hour=2024-03-05T12:00:00" # TODO check format
-    )
+    response = client.get("/stats/get_monthly_downloads?latest_hour=2026030312")
 
     assert response.status_code == HTTPStatus.OK
-    assert b"month,count" in response.data
 
 
 def test_handle_http_exception_400(client):
     response = client.get("/stats/get_monthly_downloads")
 
     assert response.status_code == 400
-    assert b"Bad Request" in response.data
-    assert b"400" in response.data
 
 
 def test_handle_http_exception_404(client):
-    response = client.get("/non-existent")
+    response = client.get("/stats/non-existent")
 
     assert response.status_code == 404
-    assert b"Not Found" in response.data
 
 
 @patch("stats_api.service.StatsService.get_downloads_page_data")
